d68de57746c8a6bcf4e27f654cf9be21dbda9c76
Szilveszter <nmszilveszter@gmail.com>
2025-05-27 15:36:38 +0200

    Initial Commit

---
+++ food/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World")
+
+def item(request):
+
+    return HttpResponse("<h1>This is an item View</h1>")
+
