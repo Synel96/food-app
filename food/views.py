c4ce1a9ca4fc7fecc39da3f50d9ede165d2b46ed
Szilveszter <nmszilveszter@gmail.com>
2025-05-28 10:06:14 +0200

    Added HTML templates

--- food/views.py
+++ food/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello World")
+    item_list = Item.objects.all()
+    context = {
+        "item_list" : item_list,
+    }
+    return render(request,"food/index.html",context)
 
 def item(request):
 
     return HttpResponse("<h1>This is an item View</h1>")
 
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        "item" : item,
+    }
+    return render(request,"food/detail.html", context)
