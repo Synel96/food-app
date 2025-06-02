7440f54d092659a3bfd780881fbf3ab39819f4e5
Szilveszter <nmszilveszter@gmail.com>
2025-06-02 10:03:08 +0200

    Updated food model with food image and index design

--- food/models.py
+++ food/models.py
@@ -10,3 +10,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    items_image = models.CharField(max_length=500,default="https://www.thefuzzyduck.co.uk/wp-content/uploads/2024/05/image-coming-soon-placeholder-01-660x660.png")
